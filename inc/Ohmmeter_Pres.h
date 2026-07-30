4924773d3f4431590f2766a446588a4402d56583
EMIC Compiler <emic-compiler@emic.io>
2026-07-30 19:56:18 +0000

    Code generation: 2026-07-30 19:56:18 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: hrd-ohmmeter-pres-1
    Project: dea-kng
    User: tomas

---
+++ inc/Ohmmeter_Pres.h
@@ -0,0 +1,31 @@
+
+#ifndef OHMMETER_Pres_H_
+#define OHMMETER_Pres_H_
+
+/*==================[inclusions]=============================================*/
+
+#include <stdint.h>
+#include "gpio.h"
+
+/*==================[macros]=================================================*/
+
+// Calibration constants (injected from module)
+#define OHM_Pres_VIN    ((float)3.0666)
+#define OHM_Pres_R1     ((float)220)
+#define OHM_Pres_ADJ    ((float)0.01)
+#define OHM_Pres_ADC_RES  ((float)1023.0)
+
+/*==================[init/poll]==============================================*/
+
+void Ohmmeter_Pres_init(void);
+
+void Ohmmeter_Pres_poll(void);
+
+/*==================[functions]=============================================*/
+
+float Ohmmeter_Pres_getResistance(void);
+
+
+/*==================[end of file]============================================*/
+#endif
+
