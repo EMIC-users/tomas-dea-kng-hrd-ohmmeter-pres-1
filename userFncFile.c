55249252b0f3c666824dc8a92a2c1cef4ee2e290
EMIC Compiler <emic-compiler@emic.io>
2026-08-12 16:51:11 +0000

    Code generation: 2026-08-12 16:51:11 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: hrd-ohmmeter-pres-1
    Project: dea-kng
    User: tomas

--- userFncFile.c
+++ userFncFile.c
@@ -21,7 +21,7 @@ void onReset()
 void etOut1()
 {
     LEDs_Led2_blink(60, 240, 1);
-    Ohmmeter_Pres_getFormattedValue(txt);
+    Ohmmeter_Pres_getFormattedValue(/* unsupported parameter type: emic-data-buffer */);
     pI2C("PRES\t%.2f", Ohmmeter_Pres_getResistance());
 }
 
